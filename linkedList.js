b188bb43b66e29397dc1172edb9cce9d6fd5016d
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-04-08 16:34:06 +0530

    feat: add append and prepend logic in linkedlist

---
+++ linkedList.js
@@ -0,0 +1,29 @@
+import Node from './node.js';
+
+export default class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        const newNode = new Node (value);
+
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    prepend(value) {
+        const newNode = new Node (value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+}
